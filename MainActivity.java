3241dcbb2b42d9b7923be30c7cfcdbfc2bb5ab26
prins_____18 <100407578+abrahamThom@users.noreply.github.com>
2024-09-07 12:10:50 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.colorchange;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.graphics.Color;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+    EditText text;
+    private ConstraintLayout mainLayout;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        mainLayout=findViewById(R.id.main);
+        button=findViewById(R.id.b1);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void click(View view) {
+
+        getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+    }
+}
