b9e4aed686d9ab34cfc74c5d6966e60ccf893fc1
prins_____18 <100407578+abrahamThom@users.noreply.github.com>
2024-09-10 19:38:49 +0530

    Add files via upload

---
+++ MainActivity (1).java
@@ -0,0 +1,30 @@
+package com.example.myproject1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+EditText s;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.t2), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        s=(EditText)findViewById(R.id.t2);
+    }
+    public void btn1(View view) {
+        s.setText("Hello World");
+    }
+}
